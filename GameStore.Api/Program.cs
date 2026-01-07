127c9e76dceea2b3b527e66085dbbc90a997b7c7
Sandeep Shrestha <sonseldeep8890.shrestha@gmail.com>
2026-01-08 02:13:33 +0545

    implemented global exception middleware, JWT authentication for Keycloak

--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -1,17 +1,52 @@
+using GameStore.Api.Database;
+using GameStore.Api.ErrorHandling;
+using GameStore.Api.Extension;
+using GameStore.Api.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Scalar.AspNetCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddProblemDetails()
+    .AddExceptionHandler<GlobalExceptionHandler>();
 
-builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddControllers(options =>
+{
+    options.ReturnHttpNotAcceptable = true;
+}).AddXmlSerializerFormatters();
+
 builder.Services.AddOpenApi();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options => 
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("Database"),
+        useSqlServer => useSqlServer.MigrationsHistoryTable(HistoryRepository.DefaultTableName,Schema.Application))
+    );
+
+builder.Services.AddScoped<IGameRepository,GameRepository>();
+builder.AddTemplateAppCors();
+builder.Services.AddAuthentication()
+    .AddJwtBearer(options =>
+    {
+        options.Authority = "http://localhost:8080/realms/gamestore-api";
+        options.Audience = "gamestore-api";
+        options.RequireHttpsMetadata = false;
+    });
+builder.Services.AddAuthorizationBuilder();
 
 var app = builder.Build();
+app.UseCors();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.MapScalarApiReference();
+    app.UseSwagger();
+    app.UseSwaggerUI();
+
+    await app.ApplyMigrationsAsync();
 }
 
 app.UseHttpsRedirection();
