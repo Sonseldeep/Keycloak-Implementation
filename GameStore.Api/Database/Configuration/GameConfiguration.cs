8ba6df8eb66bcd1fe215902c069025c58cdf18ba
Sandeep Shrestha <sonseldeep8890.shrestha@gmail.com>
2026-01-08 02:10:07 +0545

    add server validation for game entity

--- GameStore.Api/Database/Configuration/GameConfiguration.cs
+++ GameStore.Api/Database/Configuration/GameConfiguration.cs
@@ -1,6 +1,17 @@
+using GameStore.Api.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
 namespace GameStore.Api.Database.Configuration;
 
-public class GameConfiguration
+public sealed class GameConfiguration : IEntityTypeConfiguration<Game>
 {
-    
+    public void Configure(EntityTypeBuilder<Game> builder)
+    {
+        builder.HasKey(g => g.Id);
+        builder.Property(g => g.Id).IsRequired().HasMaxLength(100);
+        builder.Property(g => g.Name).IsRequired().HasMaxLength(100);
+        builder.Property(g => g.Genre).IsRequired().HasMaxLength(50);
+        builder.Property(g => g.Price).HasColumnType("decimal(18,2)");
+    }
 }
