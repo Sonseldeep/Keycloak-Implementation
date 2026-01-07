513527f5d4642cf41d7e207ca894e882b79fb98e
Sandeep Shrestha <sonseldeep8890.shrestha@gmail.com>
2026-01-08 02:03:21 +0545

    feat: add interfaces for GameRepository

--- GameStore.Api/Repositories/IGameRepository.cs
+++ GameStore.Api/Repositories/IGameRepository.cs
@@ -1,6 +1,12 @@
+using GameStore.Api.Entities;
+
 namespace GameStore.Api.Repositories;
 
 public interface IGameRepository
 {
-    
+    Task<IEnumerable<Game>> GetAllAsync(CancellationToken cancellationToken = default);
+    Task<Game?> GetByIdAsync(string id,CancellationToken cancellationToken = default);
+    Task AddAsync(Game game, CancellationToken cancellationToken = default);
+    Task UpdateAsync(Game game,CancellationToken cancellationToken = default);
+    Task DeleteAsync(Game game,CancellationToken cancellationToken = default);
 }
