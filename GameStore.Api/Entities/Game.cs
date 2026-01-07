faa6fb38415e6d1498c240d92f9d3bb818b15f7f
Sandeep Shrestha <sonseldeep8890.shrestha@gmail.com>
2026-01-08 01:59:22 +0545

    add game entities

--- GameStore.Api/Entities/Game.cs
+++ GameStore.Api/Entities/Game.cs
@@ -1,6 +1,10 @@
 namespace GameStore.Api.Entities;
 
-public class Game
+public sealed class Game
 {
-    
+    public string Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Genre { get; set; } =  string.Empty;
+    public decimal Price { get; set; }
+    public DateTime ReleaseDate { get; set; }
 }
