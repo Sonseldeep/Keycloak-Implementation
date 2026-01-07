efc736516966afc900bc37e71d4c7723ce5a61b8
Sandeep Shrestha <sonseldeep8890.shrestha@gmail.com>
2026-01-08 02:00:48 +0545

    add cors extension

--- GameStore.Api/Extension/CorsExtension.cs
+++ GameStore.Api/Extension/CorsExtension.cs
@@ -1,6 +1,32 @@
+using Microsoft.Net.Http.Headers;
+
 namespace GameStore.Api.Extension;
 
-public class CorsExtensions
+public static class CorsExtension
 {
-    
+    public static IHostApplicationBuilder AddTemplateAppCors(this IHostApplicationBuilder builder)
+    {
+        builder.Services.AddCors(options =>
+        {
+            options.AddDefaultPolicy(
+                policy =>
+                {
+                    if (builder.Environment.IsDevelopment())
+                    {
+                        policy.AllowAnyOrigin();
+                    }
+                    else
+                    {
+                        var originsString = builder.Configuration["AllowedOrigins"] ?? string.Empty;
+                        var allowedOrigins = originsString.Split(';', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+                        policy.WithOrigins(allowedOrigins);
+                    }
+
+                    policy.WithHeaders(HeaderNames.Authorization, HeaderNames.ContentType)
+                        .AllowAnyMethod();
+                });
+        });
+
+        return builder;
+    }
 }
