d849278d08d900b4b212eb01532654937c143f5d
Sandeep Shrestha <sonseldeep8890.shrestha@gmail.com>
2026-01-08 02:06:55 +0545

    dto: for create game object

--- GameStore.Api/DTOs/Game/CreateGameDTO.cs
+++ GameStore.Api/DTOs/Game/CreateGameDTO.cs
@@ -1,6 +1,9 @@
 namespace GameStore.Api.DTOs.Game;
 
-public class CreateGameDTO
+public sealed record CreateGameDto
 {
-    
+    public string Name { get; init; }
+    public string Genre { get; init; }
+    public decimal Price { get; init; }
+    public DateTime ReleaseDate { get; init; }
 }
