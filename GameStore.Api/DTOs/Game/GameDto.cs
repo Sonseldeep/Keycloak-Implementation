16260ab88bf930916bdfc931e200eda2857030eb
Sandeep Shrestha <sonseldeep8890.shrestha@gmail.com>
2026-01-08 02:07:45 +0545

    dto: game dto for get data

--- GameStore.Api/DTOs/Game/GameDto.cs
+++ GameStore.Api/DTOs/Game/GameDto.cs
@@ -1,6 +1,10 @@
 namespace GameStore.Api.DTOs.Game;
 
-public class GameDto
+public sealed record GameDto
 {
-    
+    public string Id { get; init; }
+    public string Name { get; init; }
+    public string Genre { get; init; }
+    public decimal Price { get; init; }
+    public DateTime ReleaseDate { get; init; }
 }
