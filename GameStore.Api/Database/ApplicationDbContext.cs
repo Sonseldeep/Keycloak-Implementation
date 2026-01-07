2e236f578a7215d5e7bbad9de87e2a065a0ba6d0
Sandeep Shrestha <sonseldeep8890.shrestha@gmail.com>
2026-01-08 02:01:44 +0545

    feat: add database configuration setup

--- GameStore.Api/Database/ApplicationDbContext.cs
+++ GameStore.Api/Database/ApplicationDbContext.cs
@@ -1,6 +1,16 @@
+using GameStore.Api.Entities;
+using Microsoft.EntityFrameworkCore;
+
 namespace GameStore.Api.Database;
 
-public class ApplicationDbContext
+public sealed class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
 {
+    public DbSet<Game> Games { get; set; }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.HasDefaultSchema(Schema.Application);
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+    }
+    
     
 }
