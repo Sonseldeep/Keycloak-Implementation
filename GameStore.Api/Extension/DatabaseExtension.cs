9226506b229f471f9bb2760cb35b2b35de0c0b25
Sandeep Shrestha <sonseldeep8890.shrestha@gmail.com>
2026-01-08 02:00:19 +0545

    add database migration extension

--- GameStore.Api/Extension/DatabaseExtension.cs
+++ GameStore.Api/Extension/DatabaseExtension.cs
@@ -1,6 +1,24 @@
+using GameStore.Api.Database;
+using Microsoft.EntityFrameworkCore;
+
 namespace GameStore.Api.Extension;
 
-public class DatabaseExtension
+public static class DatabaseExtension
 {
-    
+    public static async Task ApplyMigrationsAsync(this WebApplication app)
+    {
+        using var scope = app.Services.CreateScope();
+        await using var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+
+        try
+        {
+            await dbContext.Database.MigrateAsync();
+            app.Logger.LogInformation("Database migrations applied successfully");
+        }
+        catch (Exception e)
+        {
+            app.Logger.LogError(e, "Database migrations failed");
+            throw;
+        }
+    }
 }
